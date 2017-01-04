08ef8da3917f640a8fe679b869d9af3f80c5d11f
Sylphias <sylpharyon@gmail.com>
2017-01-04 14:16:29 +0800

    Write a running script to keep running on orchestrating machine

--- radamsa.py
+++ radamsa.py
@@ -1,8 +1,8 @@
 import subprocess,json,requests,time,random
+
 # This method is used to generate the fuzzing inputs to be used on the endpoint of the device. note that it can be customized based on a set of input list
-def generate_hue_inputs():
-    # this is to store the types of input that we will be fuzzing and generate variations of these inputs
-    inputs_list = {"bri":"123", "on": "True","hue":"12223", "sat":"123"}
+def generate_hue_inputs(input_list):
+
     #  URL and token for local access
     # http: // 192.168.2.139/api/zLcVDH439gTV3VGebD-s7XhS4DTvAAupN7VDGhIw/lights/1/state
     hue_inputs = {}
@@ -10,44 +10,46 @@ def generate_hue_inputs():
     for key,value in inputs_list.iteritems():
         # Run radamsa fuzzer
         p1 = subprocess.Popen(["echo",str(inputs_list[key])],stdout= subprocess.PIPE)
-        proc = subprocess.Popen(["radamsa","-n","1000"],stdin=p1.stdout, stdout=subprocess.PIPE)
+        proc = subprocess.Popen(["radamsa"],stdin=p1.stdout, stdout=subprocess.PIPE)
         p1.stdout.close();
         (out, err) = proc.communicate()
-        # Now we have a thousand fuzzed inputs to pump into the HUE to test.
-        hue_inputs[key] = out.splitlines()
+        hue_inputs[key] = out
     return hue_inputs
 
 
-def send_hue_packet(url,endpoint,option):
-    fuzz_data = generate_hue_inputs();
-    f = open("fuzzlog.txt", "a+")
-    headers = {'Content-Type': 'application/json',
-               'X-Token': 'M0htalVOQTlFSldxWlYyMnJzbzVOeVFmdUFIWDRhL0N3REJyYXF3UFZqaz0='}
-    data = {}
-    for count in range(0,1000):
-        # Create a data packet to send to the hue api
-        light_no = random.randint(1,3)
-        for keys in fuzz_data.keys():
-            data[keys] = fuzz_data[keys][count]
-        r = requests.put(url+endpoint+str(light_no)+option, data=data, headers=headers)
-        f.write("\n-----------------NEW REQUEST----------------\n\n")
-        f.write(url+endpoint+str(light_no)+option + "\ndata = " + str(data) + "\nheaders = " + str(headers) + "\n")
-        f.write("\n-----------------RESPONSE----------------\n")
-        if type(r) is list:
-            for item in r:
-                for key,value in item:
-                    f.write(key + " : " + value + "\n")
-        else:
-            f.write(str(r) + ' : ' + r.content + "\n");
-        time.sleep(2)
+def send_hue_packet(url,endpoint,option,inputs_list,headers = {}):
+    fuzz_data = generate_hue_inputs(inputs_list);
+    f = open("fuzzlog_all.txt", "a+")
+    s = open("fuzzlog_success.txt", "a+")
+    # Create a data packet to send to the hue api
+    # light_no = random.randint(1,3)
+    r = requests.put(url+endpoint+"1"+option, data=json.dumps(fuzz_data,ensure_ascii=False), headers=headers)
+    decoded_response = json.JSONDecoder().decode(r.text)
+    has_success = False
+    for item in decoded_response:
+        has_success = not 'error' in item
+    if (r.status_code == 200 and has_success) or r.status_code != 200:
+        write_format(s,url,endpoint,option,fuzz_data,headers)
+        for item in r:
+            for key,value in item:
+                s.write(key + " : " + value + "\n")
+    write_format(f, url, endpoint, option, fuzz_data, headers)
+    f.write(str(r) + ' : ' + r.content + "\n");
+
+# Keeping the code DRY
+def write_format(file_input,url,endpoint,option,data,headers):
+    file_input.write("\n-----------------NEW REQUEST----------------\n\n")
+    file_input.write(url + endpoint + "1" + option + "\ndata = " + str(data) + "\nheaders = " + str(headers) + "\n")
+    file_input.write("\n------------------RESPONSE------------------\n")
+
 
 def party_lights():
-    # headers = {'Content-Type': 'application/json'}
     data = {}
     for count in range(0,1000):
         # Create a data packet to send to the hue api
         data["hue"] = random.randint(0, 65535)
         data["sat"] = random.randint(0, 255)
+        data["on"] = True
         light_no = random.randint(1,3)
         r = requests.put('http://192.168.2.139/api/zLcVDH439gTV3VGebD-s7XhS4DTvAAupN7VDGhIw/lights/'+str(light_no)+'/state', data=json.dumps(data))
         time.sleep(0.01)
@@ -56,36 +58,15 @@ def party_lights():
 
 
 if __name__ == "__main__" :
-    send_hue_packet('https://client.meethue.com/','api/0/lights/','/state')
-
+    # this is to store the types of input that we will be fuzzing and generate variations of these inputs
+    while(True):
+        try:
+            hue = random.randint(0, 65535)
+            sat = random.randint(0, 255)
+            bri = random.randint(0, 255)
+            on = bool(random.getrandbits(1))
+            inputs_list = {"bri": bri, "on": on, "hue": hue, "sat": sat}
+            send_hue_packet('http://192.168.2.139/', 'api/zLcVDH439gTV3VGebD-s7XhS4DTvAAupN7VDGhIw/lights/', '/state',inputs_list)
+        except KeyboardInterrupt:
+            pass
     # party_lights()
-    # while(True):
-    #     try:
-    #         send_hue_packet()
-    #     except KeyboardInterrupt:
-    #         pass
-
-
-
-# import subprocess
-#
-# proc = subprocess.Popen(["radamsa", ])
-# (out, err) = proc.communicate()
-# print "program output:", out
-#
-# p1 = Popen(["dmesg"], stdout=PIPE)
-# p2 = Popen(["grep", "hda"], stdin=p1.stdout, stdout=PIPE)
-# p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
-# output = p2.communicate()[0]
-#
-# # Authentication request setup
-#         data = {'grant_type': 'client_credentials'}
-#         headers = {                                                   \
-#                 'content-type' : 'application/x-www-form-urlencoded', \
-#                 'Authorization': 'Basic ' + encodedSecurityInfo       \
-#                 }
-#
-#         # Request authentication
-#         data = urllib.urlencode(data)
-#         request = urllib2.Request(config.auth_url, data, headers)
-#         response = json.loads(urllib2.urlopen(request).read())
