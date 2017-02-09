7414a2da6ff2a39efe38226a78a6b50d7a85af37
Sylphias <sylpharyon@gmail.com>
2017-02-09 16:08:28 +0800

    Added multithreaded capabilities

--- radamsa.py
+++ radamsa.py
@@ -1,4 +1,27 @@
-import subprocess,json,requests,time,random,datetime,sys,ast,re
+import subprocess,json,requests,time,random,datetime,sys,ast,re, thread, threading
+
+
+class RadamsaThread(threading.Thread):
+    def __init__(self,number):
+        threading.Thread.__init__(self)
+        self.number = number
+
+    def stop(self):
+        self._Thread__stop()
+
+    #There is a certain amout of time needed for radamsa to run and produce a full output. so dont set the timer to be too low.
+    def run(self):
+        threadlock = threading.Lock()
+        threading.Timer(0.1,self.run).start()
+        threadlock.acquire()
+        hue = random.randint(0, 65535)
+        sat = random.randint(0, 255)
+        bri = random.randint(0, 255)
+        on = bool(random.getrandbits(1))
+        print("FuzzThread "+ str(self.number) + " Is Running")
+        inputs_list = {"bri": bri, "on": on, "hue": hue, "sat": sat}
+        sendFuzzPacketOnly('http://192.168.2.139/', 'api/zLcVDH439gTV3VGebD-s7XhS4DTvAAupN7VDGhIw/lights/', '/state',inputs_list)
+        threadlock.release()
 
 # This method is used to generate the fuzzing inputs to be used on the endpoint of the device. note that it can be customized based on a set of input list
 def generate_hue_inputs(input_list):
@@ -19,13 +42,13 @@ def generate_hue_inputs(input_list):
 def send_hue_packet(url,endpoint,option,inputs_list,timestamp,headers = {}):
     fuzz_data = generate_hue_inputs(inputs_list);
     all_log_file = open("fuzzlog_" + timestamp + ".txt", "a+")
-    success_log_file = open("fuzzlog_success"+ timestamp +".txt", "a+")
-    # Create a data packet to send to the hue api
-    # light_no = random.randint(1,3)
+    outlier_log_file = open("fuzzlog_failures"+ timestamp +".txt", "a+")
     try:
         r = requests.put(url + endpoint + "1" + option, data=json.dumps(fuzz_data, ensure_ascii=False), headers=headers)
     except:
-        all_log_file.write("{'url':\'" + url + endpoint + "1" + option + "\','data': " + str(fuzz_data) + ",'headers':" + str(headers) + ", 'contents' : CONNECTION ERROR >> "+ str(sys.exc_info()[0].message) +"}\n")
+        connection_log = "{'url':\'" + url + endpoint + "1" + option + "\','data': " + str(fuzz_data) + ",'headers':" + str(headers) + ", 'contents' : CONNECTION ERROR >> "+ str(sys.exc_info()[0].message) +"}\n"
+        all_log_file.write(connection_log)
+        outlier_log_file.write(connection_log)
         return
     try:
         decoded_response = json.JSONDecoder().decode(r.text)
@@ -36,18 +59,40 @@ def send_hue_packet(url,endpoint,option,inputs_list,timestamp,headers = {}):
     for item in decoded_response:
         has_success = not 'error' in item
     if (r.status_code == 200 and has_success) or r.status_code != 200:
-        write_format(success_log_file,url,endpoint,option,fuzz_data,headers)
+        write_format(outlier_log_file,url,endpoint,option,fuzz_data,headers)
         for item in r:
             for key,value in item:
-                success_log_file.write(key + " : " + value + "\n")
+                outlier_log_file.write(key + " : " + value + "\n")
     # Removed human readability from the log so I can easily parse the logfiles through Python to display
     all_log_file.write("{'url':\"" + url + endpoint + "1" + option + "\",'data': " + str(fuzz_data) + ",'headers':" + str(headers)+", 'contents' : " + r.content  + "}\n")
+
+def sendFuzzPacketOnly(url,endpoint,option,inputs_list,headers = {}):
+    fuzz_data = generate_hue_inputs(inputs_list);
+    try:
+        r = requests.put(url + endpoint + "1" + option, data=json.dumps(fuzz_data, ensure_ascii=False), headers=headers)
+    except requests.exceptions.RequestException as e:
+        print e.message
+
+
+
+def spawn_multiple_instances(noOfThread):
+    fuzz_threads =[]
+    counter = 0
+    for threads in range(noOfThread):
+        fuzz_threads += [RadamsaThread(counter)]
+        counter +=1
+    for fuzzThread in fuzz_threads:
+        fuzzThread.run()
+    return fuzz_threads
+
 
 
 def write_format(file_input,url,endpoint,option,data,headers):
     file_input.write("\n-----------------NEW REQUEST----------------\n")
     file_input.write(url + endpoint + "1" + option + "\ndata = " + str(data) + "\nheaders = " + str(headers) + "\n")
     file_input.write("\n------------------RESPONSE------------------\n")
+
+
 
 
 def generate_excel_format(timestamp):
@@ -82,16 +127,23 @@ def generate_excel_format(timestamp):
 
 if __name__ == "__main__" :
     # this is to store the types of input that we will be fuzzing and generate variations of these inputs
-    # timestart = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m%-d%H%M%S')
-    # while(True):
-    #     try:
-    #         hue = random.randint(0, 65535)
-    #         sat = random.randint(0, 255)
-    #         bri = random.randint(0, 255)
-    #         on = bool(random.getrandbits(1))
-    #         inputs_list = {"bri": bri, "on": on, "hue": hue, "sat": sat}
-    #         send_hue_packet('http://192.168.2.139/', 'api/zLcVDH439gTV3VGebD-s7XhS4DTvAAupN7VDGhIw/lights/', '/state',inputs_list,timestart)
-    #     except (KeyboardInterrupt, SystemExit):
-    #         generate_excel_format(timestart)
-    #         break
-    generate_excel_format("2017-0124143306")
+    timestart = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m%-d%H%M%S')
+    fuzz_threads = spawn_multiple_instances(4)
+
+    while(True):
+        try:
+            hue = random.randint(0, 65535)
+            sat = random.randint(0, 255)
+            bri = random.randint(0, 255)
+            on = bool(random.getrandbits(1))
+            inputs_list = {"bri": bri, "on": on, "hue": hue, "sat": sat}
+            send_hue_packet('http://192.168.2.139/', 'api/zLcVDH439gTV3VGebD-s7XhS4DTvAAupN7VDGhIw/lights/', '/state',inputs_list,timestart)
+        except (KeyboardInterrupt, SystemExit):
+            # threadlock = threading.Lock()
+            # threadlock.acquire()
+            generate_excel_format(timestart)
+            for fuzzThread in fuzz_threads:
+                fuzzThread.stop()
+            # threadlock.release()
+            break
+    # generate_excel_format("2017-028203243")
